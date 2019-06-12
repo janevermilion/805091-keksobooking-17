443a090c695b453147074ab6d760ceeca7c48d51
Janevermilion <44806521+janevermilion@users.noreply.github.com>
2019-06-12 17:33:52 +0300

    исправление отрисовки

--- js/main.js
+++ js/main.js
@@ -2,49 +2,57 @@
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
 var screenWidth = document.documentElement.clientWidth;
-var numberOfLocations = 8;
+var NUMBER_OF_LOCATIONS = 8;
 var types = ['palace', 'flat', 'house', 'bungalo'];
-var templatePin = document.getElementById('pin').content.querySelector('.map__pin');
+var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
+var WIDTH_OF_PIN = 50;
+var HEIGHT_OF_PIN = 70;
 var getRandomNumber = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   rand = Math.floor(rand);
   return rand;
 };
-var getAvatarAdresses = function () {
-  var listOfAvatars = [];
-  for (var i = 1; i <= numberOfLocations; i++) {
+var getAvatarAdresses = function (locations) {
+  var listOfAdresses = [];
+  for (var i = 1; i <= locations; i++) {
     var avatarAdress = 'img/avatars/user' + '0' + i + '.png';
-    listOfAvatars.push(avatarAdress);
+    listOfAdresses.push(avatarAdress);
   }
-  return listOfAvatars;
+  return listOfAdresses;
 };
-var listOfAvatars = getAvatarAdresses();
+var adresses = getAvatarAdresses(NUMBER_OF_LOCATIONS);
 var createObject = function (avatar, type, x, y) {
   var object = {};
   object.author = avatar;
   object.offer = type;
-  object.location = {x,y};//eslint не нравится такая запись
+  object.location = {
+    x: x,
+    y: y
+  };
 
   return object;
 };
-var getObjectsArray = function () {
-  var objects = [];
+var getObjectsArray = function (numberOfLocations) {
+  var objectsArray = [];
   for (var i = 0; i < numberOfLocations; i++) {
-    var newObject = createObject(listOfAvatars[i], types[getRandomNumber(0, types.length - 1)], getRandomNumber(0, screenWidth), getRandomNumber(130, 630));
-    objects.push(newObject)
+    var newObject = createObject(adresses[i], types[getRandomNumber(0, types.length - 1)], getRandomNumber(0, screenWidth), getRandomNumber(130, 630));
+    objectsArray.push(newObject);
   }
-  return objects;
+  return objectsArray;
 };
-var objects = getObjectsArray();
-var addNewPins = function () {
-  for (var i = 0; i < numberOfLocations; i++) {
+var objects = getObjectsArray(NUMBER_OF_LOCATIONS);
+var addNewPins = function (array) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < array.length; i++) {
     var newPin = templatePin.cloneNode(true);
-    newPin.style.left = (objects[i].location.x - 50) + 'px';
-    newPin.style.top = (objects[i].location.y - 70) + 'px';
-    var pinImg = newPin.getElementsByTagName('img')[0];
-    pinImg.alt = objects[i].offer;
-    pinImg.src = objects[i].author;
-    map.appendChild(newPin);
+    newPin.style.left = (array[i].location.x + WIDTH_OF_PIN / 2) + 'px';
+    newPin.style.top = (array[i].location.y - HEIGHT_OF_PIN) + 'px';
+    var pinImg = newPin.querySelector('img');
+    pinImg.alt = array[i].offer;
+    pinImg.src = array[i].author;
+    fragment.appendChild(newPin);
   }
+  return fragment;
 };
-addNewPins();
+var listOfNewPins = addNewPins(objects);
+map.appendChild(listOfNewPins);

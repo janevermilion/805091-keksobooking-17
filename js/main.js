79a6170404d2555fd9b1dafcabea3fa6d459e07f
Janevermilion <44806521+janevermilion@users.noreply.github.com>
2019-06-21 17:52:42 +0300

    перемещение пина

--- js/main.js
+++ js/main.js
@@ -3,12 +3,10 @@ var MAP_WIDTH = 1200;
 var NUMBER_OF_LOCATIONS = 8;
 var WIDTH_OF_PIN = 50;
 var HEIGHT_OF_PIN = 70;
-var HEIGHT_OF_START_PIN = 84;
-var WIDTH_OF_START_PIN = 62;
 var MAP_HEIGHT = 704;
 var MIN_PRICES = ['0', '1000', '5000', '10000'];
 var HOUSING_TYPES = ['bungalo', 'flat', 'house', 'palace'];
-var map = document.querySelector('.map');
+var map = document.querySelector('.map__pins');
 var templatePin = document.querySelector('#pin').content.querySelector('.map__pin');
 var getRandomNumber = function (min, max) {
   var rand = min + Math.random() * (max + 1 - min);
@@ -32,7 +30,6 @@ var createObject = function (avatar, type, x, y) {
     x: x,
     y: y
   };
-
   return object;
 };
 var getObjectsArray = function (numberOfLocations) {
@@ -61,46 +58,25 @@ var getPinsFragment = function (array) {
   }
   return fragment;
 };
-
-var startPin = document.querySelector('.map__pin--main');
+var mainPin = document.querySelector('.map__pin--main');
 var adForm = document.querySelector('.ad-form');
 var filtersForm = document.querySelector('.map__filters');
-
 var adFormFieldsetList = adForm.querySelectorAll('fieldset');
-
 var addDisableAttr = function (list) {
   for (var i = 0; i < list.length; i++) {
     list[i].disabled = true;
   }
 };
-
 addDisableAttr(adFormFieldsetList);
 var filtersFormChildrenList = filtersForm.querySelectorAll('fieldset, select');
 addDisableAttr(filtersFormChildrenList);
-
 var deleteDisableAttr = function (list) {
   for (var i = 0; i < list.length; i++) {
     list[i].disabled = false;
   }
 };
-
-var pinClickHandler = function () {
-  map.classList.remove('map--faded');
-  var pinsFragment = getPinsFragment(pinsData);
-  map.appendChild(pinsFragment);
-  adForm.classList.remove('ad-form--disabled');
-  deleteDisableAttr(adFormFieldsetList);
-  deleteDisableAttr(filtersFormChildrenList);
-  resetFormButton.addEventListener('click', formClearHandler);
-  typeOfHouse.addEventListener('change', typeOfHouseSelectHandler);
-  checkInTime.addEventListener('change', checkInSelectHandler);
-  checkOutTime.addEventListener('change', checkOutSelectHandler);
-  startPin.removeEventListener('click', pinClickHandler);
-};
-
-startPin.addEventListener('click', pinClickHandler);
 var adressInput = adForm.querySelector('[name="address"]');
-adressInput.value = (MAP_WIDTH / 2 - WIDTH_OF_START_PIN / 2).toString() + ' , ' + (MAP_HEIGHT / 2 - HEIGHT_OF_START_PIN / 2).toString();
+// adressInput.value = (MAP_WIDTH / 2 - WIDTH_OF_START_PIN / 2).toString() + ' , ' + (MAP_HEIGHT / 2 - HEIGHT_OF_START_PIN / 2).toString();
 var resetFormButton = adForm.querySelector('.ad-form__reset');
 var formClearHandler = function () {
   map.classList.add('map--faded');
@@ -112,17 +88,15 @@ var formClearHandler = function () {
   typeOfHouse.removeEventListener('change', typeOfHouseSelectHandler);
   checkInTime.removeEventListener('change', checkInSelectHandler);
   checkOutTime.removeEventListener('change', checkOutSelectHandler);
-  startPin.addEventListener('click', pinClickHandler);
-
+  mainPin.style.left = 570 + 'px';
+  mainPin.style.top = 375 + 'px;'
 };
-
 var clearPins = function () {
   var pins = document.querySelectorAll('.map__pin');
   for (var i = 1; i < pins.length; i++) {
     pins[i].remove();
   }
 };
-
 var typeOfHouse = adForm.querySelector('#type');
 var priceForNight = adForm.querySelector('#price');
 var checkInTime = adForm.querySelector('#timein');
@@ -132,12 +106,60 @@ var typeOfHouseSelectHandler = function () {
   priceForNight.placeholder = MIN_PRICES[index];
   priceForNight.min = MIN_PRICES[index];
 };
-
 var checkInSelectHandler = function () {
   checkOutTime.value = checkInTime.value;
 };
 var checkOutSelectHandler = function () {
   checkInTime.value = checkOutTime.value;
 };
-
-
+var mainPinMousedownHandler = function () {
+}
+mainPin.addEventListener('mousedown', function (drugEvt) {
+  //var mainPinMousedownHandler = function(drugEvt) {
+  drugEvt.preventDefault();
+  var startCoords = {
+    x: drugEvt.clientX,
+    y: drugEvt.clientY
+  };
+  var mainPinMousemoveHandler = function (moveEvt) {
+    var MAIN_PIN_RADIUS = 130;
+    moveEvt.preventDefault();
+    mainPin.draggable = true;
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+    if ((mainPin.offsetTop - shift.y) > MAIN_PIN_RADIUS && (mainPin.offsetTop - shift.y) < 650) {
+      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+    } else {
+      mainPin.style.top = MAIN_PIN_RADIUS
+    };
+    if ((mainPin.offsetLeft - shift.x) > MAIN_PIN_RADIUS && (mainPin.offsetLeft - shift.x) < (MAP_WIDTH - 130)) {
+      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+    } else {
+      mainPin.style.left = MAIN_PIN_RADIUS;
+    }
+  };
+  var mainPinMouseupHander = function (upEvt) {
+    upEvt.preventDefault();
+    adressInput.value = mainPin.offsetLeft + ',' + mainPin.offsetTop;
+    map.classList.remove('map--faded');
+    var pinsFragment = getPinsFragment(pinsData);
+    map.appendChild(pinsFragment);
+    adForm.classList.remove('ad-form--disabled');
+    deleteDisableAttr(adFormFieldsetList);
+    deleteDisableAttr(filtersFormChildrenList);
+    resetFormButton.addEventListener('click', formClearHandler);
+    typeOfHouse.addEventListener('change', typeOfHouseSelectHandler);
+    checkInTime.addEventListener('change', checkInSelectHandler);
+    checkOutTime.addEventListener('change', checkOutSelectHandler);
+    document.removeEventListener('mousemove', mainPinMousemoveHandler);
+    document.removeEventListener('mouseup', mainPinMouseupHander);
+  };
+  document.addEventListener('mousemove', mainPinMousemoveHandler);
+  document.addEventListener('mouseup', mainPinMouseupHander);
+});

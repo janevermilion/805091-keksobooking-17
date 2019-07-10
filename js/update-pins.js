fcb03b4894191b6874eb2326e89e51ba73525af8
Janevermilion <44806521+janevermilion@users.noreply.github.com>
2019-07-10 20:20:49 +0300

    предзащита

--- js/update-pins.js
+++ js/update-pins.js
@@ -1,36 +1,46 @@
 'use strict';
 (function () {
-  var housingTypeFilter = document.querySelector('#housing-type');
 
-  housingTypeFilter.addEventListener('change', function () {
+  var filterForm = document.querySelector('.map__filters');
+  filterForm.addEventListener('change', function (evt) {
+    var selectedValue = evt.target;
     window.debounce(function () {
-      window.updatePins();
+      window.updatePins(selectedValue);
     });
   });
 
-  window.updatePins = function () {
+
+  window.updatePins = function (filterData) {
+    var typeOfData = filterData.name.slice(8).toString();
 
     window.pinsActions.clear();
     var filteredHouses;
-    if (housingTypeFilter.value === 'any') {
+
+    if (filterData.value === 'any') {
       filteredHouses = window.pinsData;
+    }
+    if (typeOfData === 'price') {
+      filteredHouses = window.pinsData.filter(function (it) {
+        if (filterData.value === 'middle') {
+          return it.offer.price > 10000 && it.offer.price < 50000;
+        }
+        if (filterData.value === 'low') {
+          return it.offer.price < 10000;
+        }
+        if (filterData.value === 'high') {
+          return it.offer.price > 50000;
+        }
+        return filteredHouses;
+      });
     } else {
       filteredHouses = window.pinsData.filter(function (it) {
-        return it.offer.type === housingTypeFilter.value;
+        return it.offer[typeOfData] === filterData.value;
       });
     }
 
     window.pinsActions.render(filteredHouses);
 
   };
-
-/* var filterForm = document.querySelector('.map__filters');
-  var filterValues = [];
-  filterForm.addEventListener('change', function(evt){
-  var selectedValue = evt.target.value;
-  filterValues.push(selectedValue)
-
-}) */
 
 })();
 
